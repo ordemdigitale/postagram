f9063d3bb1062d18c058c0275e2e0a980b61fd67
ordemdigitale <ordemdigitale@gmail.com>
2023-03-16 17:32:34 +0000

    User model

---
+++ core/user/models.py
@@ -0,0 +1,31 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+from django.http import Http404
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(db_index=True, unique=True,
+                                default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True,
+                                max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return f"{self.email}"
+    
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
