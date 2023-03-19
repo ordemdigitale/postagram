56ca05b539e26a719d6ff4bf09401fdf8cd42aae
ordemdigitale <ordemdigitale@gmail.com>
2023-03-19 16:06:17 +0000

    Auth logic

--- core/routers.py
+++ core/routers.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
+from core.auth.viewsets.register import RegisterViewSet
 
 router = routers.SimpleRouter()
 
@@ -11,3 +12,8 @@ router.register(r'user', UserViewSet, basename='user')
 urlpatterns = [
     *router.urls,
 ]
+
+# ##################################################################### #
+# ################### AUTH ###################### #
+# ##################################################################### #
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
