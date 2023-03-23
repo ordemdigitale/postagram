54f3bf192c183e47815cd756a78d72484067f102
ordemdigitale <ordemdigitale@gmail.com>
2023-03-23 15:33:11 +0000

    Post app & surrounded apps label with double quotes

--- core/post/apps.py
+++ core/post/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class UserConfig(AppConfig):
+class PostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.user'
-    label = "core_user"
+    name = 'core.post'
+    label = "core_post"
