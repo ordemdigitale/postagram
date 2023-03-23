b5424b21b173d54b05747ae9c2fe898741794ef9
ordemdigitale <ordemdigitale@gmail.com>
2023-03-23 19:57:20 +0000

    Like feature

--- core/user/models.py
+++ core/user/models.py
@@ -41,22 +41,36 @@ class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    
+
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    
+
     bio = models.CharField(max_length=150, blank=True, null=True)
     avatar = models.ImageField(upload_to='images/user_avatar', blank=True, null=True)
-    
+
+    posts_liked = models.ManyToManyField("core_post.Post", related_name="liked_by")
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    
+
     objects = UserManager()
-    
+
     def __str__(self):
         return f"{self.email}"
-    
+
     @property
     def name(self):
         return f"{self.first_name} {self.last_name}"
+    
+    def like(self, post):
+        """Like `post` if it hasn't been done yet"""
+        return self.posts_liked.add(post)
+    
+    def remove_like(self, post):
+        """Remove a like from a `post`"""
+        return self.posts_liked.remove(post)
+    
+    def has_liked(self, post):
+        """Return True if the user has liked a `post`, else False"""
+        return self.posts_liked.filter(pk=post.pk).exists()
