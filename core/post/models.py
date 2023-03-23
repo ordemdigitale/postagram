67595981d969bb1c1411cc449dbf609a587bcd93
ordemdigitale <ordemdigitale@gmail.com>
2023-03-23 15:42:05 +0000

    Post model

--- core/post/models.py
+++ core/post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from core.abstract.models import AbstractManager, AbstractModel
+
+
+class PostManager(AbstractManager):
+    pass
+
+
+class Post(AbstractModel):
+    author = models.ForeignKey(to="core_user.User", on_delete=models.CASCADE)
+    body = models.TextField()
+    edited = models.BooleanField(default=False)
+
+    objects = PostManager()
+
+    def __str__(self):
+        return f"{self.author.name}"
+    
+#    class Meta:
+#        db_table = "'core.post'"
