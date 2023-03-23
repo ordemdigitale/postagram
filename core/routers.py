926391cc5174943ec6b858ed32b987ceae6399a8
ordemdigitale <ordemdigitale@gmail.com>
2023-03-23 16:21:46 +0000

    Post endpoint

--- core/routers.py
+++ core/routers.py
@@ -1,19 +1,22 @@
 from rest_framework import routers
+
+from core.post.viewsets import PostViewSet
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 
 router = routers.SimpleRouter()
 
-# ##################################################################### #
-# ################### USER ###################### #
-# ##################################################################### #
-router.register(r'user', UserViewSet, basename='user')
-# ##################################################################### #
+
 # ################### AUTH ###################### #
-# ##################################################################### #
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+# ################### USER ###################### #
+router.register(r'user', UserViewSet, basename='user')
+
+# ################### POST ###################### #
+router.register(r'post', PostViewSet, basename='post')
 
 urlpatterns = [
     *router.urls,
