6175973c54016c7c4c3a43f7a173a685e549132a
ordemdigitale <ordemdigitale@gmail.com>
2023-03-23 11:17:20 +0000

    Refresh token logic

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets import RegisterViewSet, LoginViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 
 router = routers.SimpleRouter()
 
@@ -13,6 +13,7 @@ router.register(r'user', UserViewSet, basename='user')
 # ##################################################################### #
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 urlpatterns = [
     *router.urls,
